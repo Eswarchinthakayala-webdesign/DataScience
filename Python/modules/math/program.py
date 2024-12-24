7ef425dc625f3e6b9ce69f7042dc1419d6fe7970
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-12-24 12:56:08 +0530

    Create program.py

---
+++ Python/modules/math/program.py
@@ -0,0 +1,12 @@
+from math import *
+print(dir())
+
+print("square root: ",sqrt(16))
+print("Power: ",pow(2,5))
+print("log value: ",log(100,10))
+print("log value with x: ",log10(10000))
+print("sin value : ",sin(pi/4))
+print("Tan value : ",tan(pi/4))
+print("Exponential : ",exp(2))
+print("Factorial : ",factorial(10))
+print()
